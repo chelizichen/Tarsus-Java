34c5f007a4e3b95b2eec40530c524d1599b2105c
chelizichen <1347290221@qq.com>
2023-03-19 22:26:23 +0800

    update: 添加List测试

--- src/main/java/com/tarsus/example/register/Hello.java
+++ src/main/java/com/tarsus/example/register/Hello.java
@@ -1,5 +1,7 @@
 package com.tarsus.example.register;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.tarsus.example.base.TarsusBaseInterFace;
 import com.tarsus.example.base.TarsusEvents;
 import com.tarsus.example.config.ret;
@@ -12,6 +14,7 @@ import com.tarsus.example.enity.Job;
 import com.tarsus.example.service.HelloService;
 
 import java.util.HashMap;
+import java.util.List;
 
 @TarsusInterFace(interFace = "HelloInterFace")
 public class Hello extends TarsusBaseInterFace {
@@ -26,11 +29,14 @@ public class Hello extends TarsusBaseInterFace {
         return ret.success(d1);
     }
 
+    // 测试 List
     @TarsusMethod
-    public ret say(String args1,String args2){
+    public ret say(String args1, String args2, String args3){
         HashMap<String, String> hmp = new HashMap();
         hmp.put("d",args1);
         hmp.put("f",args2);
+        List jsonObject = JSON.parseArray(args3,String.class);
+        System.out.println(jsonObject.get(1));
         ret success = ret.success(hmp);
         return success;
     }
