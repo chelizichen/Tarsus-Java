e58a8bf377b3ae24f078d06622ff8bbba790e4fb
chelizichen <1347290221@qq.com>
2023-03-21 14:38:41 +0800

    update:生成Java接口

---
+++ src/main/java/com/tarsus/example/taro/TaroInterFace.java
@@ -0,0 +1,6 @@
+ package com.tarsus.example.taro;
+public interface  TaroInterFace {
+  public int getUserById(GetUserByIdReq req, GetUserByIdRes res);
+public int getUserList(GetUserListReq req, GetUserListRes res);
+
+}
